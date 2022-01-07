e777d2f552edeccbf5786e781d68b233e04edd7a
shengzhelin <do503200@gmail.com>
2022-01-08 05:41:55 +0800

    first version

---
+++ app/i18n/zh/router.js
@@ -0,0 +1,88 @@
+const router={
+  '/dashboard':'首页',
+  '/srcManage':'资源管理',
+  '/host':'主机',
+  '/groups':'主机组',
+  '/templates':'模版',
+  '/graph':'图形管理',
+  '/report':'指标报表',
+  '/export':'指标导出',
+  '/inspect':'巡检报告',
+  '/batch':'批量导出',
+  '/alarm':'告警报表',
+  '/analysis':'告警分析',
+  '/query':'告警查询',
+  '/system':'系统管理',
+  '/license':'授权訊息',
+  '/chpwd':'修改密码',
+  '/login':'登录',
+  '/signup':'注册',
+  '/404':'404',
+};
+
+export default router;
+const router={
+  '/dashboard':'首頁',
+  '/srcManage':'資源管理',
+  '/host':'主機',
+  '/groups':'主機組',
+  '/templates':'模版',
+  '/graph':'圖形管理',
+  '/report':'指標報表',
+  '/export':'指標導出',
+  '/inspect':'巡檢報告',
+  '/batch':'批量導出',
+  '/alarm':'告警報表',
+  '/analysis':'告警分析',
+  '/query':'告警查詢',
+  '/system':'系統管理',
+  '/license':'授權訊息',
+  '/chpwd':'修改密碼',
+  '/login':'登入',
+  '/signup':'註冊',
+  '/404':'404',
+};
+
+export default router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/i18n/zh/template.js
@@ -0,0 +1,15 @@
+const template = {
+  tpl: {
+    id: 'ID',
+    templates: '模版',
+    applications: '應用',
+    items: '指標',
+    triggers: '觸發器',
+    graphs: '圖形',
+    screens: '大盤',
+    discoveries: '自動發現',
+    hosts: '主機',
+  },
+};
+
+export default template;
